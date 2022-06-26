9d2fa899adcd20abc63536625fd34c797478a527
Brendan Seabrook <brendanseabrook>
2022-06-26 16:29:22 -0400

    Test

--- Sources/TabManager/Views/TabManagerView.swift
+++ Sources/TabManager/Views/TabManagerView.swift
@@ -12,11 +12,6 @@ public struct TabManagerView<TabTypes: Tabbable, Content: View>: View {
     @ObservedObject var manager: TabManager<TabTypes>
     @ViewBuilder var content: () -> Content
     
-    public init(manager: TabManager<TabTypes>, @ViewBuilder content: @escaping () -> Content) {
-        self.manager = manager
-        self.content = content
-    }
-    
     public var body: some View {
         TabView(selection: $manager.selection) {
             content()
